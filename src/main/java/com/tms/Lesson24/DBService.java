1f300704b32245b8dd7d1285bec861062d37b2d2
Mihail <MoyeyukMihail@gmail.com>
2022-09-29 12:17:36 +0300

    Lesson24refactored

---
+++ src/main/java/com/tms/Lesson24/DBService.java
@@ -0,0 +1,29 @@
+package com.tms.Lesson24;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBService {
+
+    private DBService() {
+    }
+
+    public static synchronized Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/postgres",
+                    "postgres",
+                    "3476"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/com/tms/Lesson24/LoginServlet.java
+++ src/main/java/com/tms/Lesson24/LoginServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.*;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -16,45 +15,17 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
-        Connection connection = connectionToDatabase();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select login, password, role from users");
-            while (resultSet.next()) {
-                if (resultSet.getString("login").equals(login)
-                        && resultSet.getString("password").equals(password)) {
-                    if (resultSet.getString("role").equals("admin")) {
-                        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/AddUser.html");
-                        requestDispatcher.forward(req, resp);
-                    } else if (resultSet.getString("role").equals("user")) {
-                        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/ShowImage.html");
-                        requestDispatcher.forward(req, resp);
-                    }
-                }
-            }
+        String access = LoginService.accessFilter(login, password);
+
+        if (access.equals("admin")) {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/AddUser.html");
+            requestDispatcher.forward(req, resp);
+        } else if (access.equals("user")) {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/ShowImage.html");
+            requestDispatcher.forward(req, resp);
+        } else {
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/ErrorPage.html");
             requestDispatcher.forward(req, resp);
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-
-    }
-
-    protected Connection connectionToDatabase() {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            return DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/postgres",
-                    "postgres",
-                    "3476"
-            );
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
